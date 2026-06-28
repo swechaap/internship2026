1e935118b4af5be55ba2b5b949e1d9c8f1888139
divya2912-diya <divyasreeb29@gmail.com>
2026-06-28 09:00:08 +0530

    Create confug.js

---
+++ workplace-policies-labor-laws-and-employee-rights-platform-e06/confug.js
@@ -0,0 +1,5 @@
+// Supabase configuration for WorkRights Hub
+window.ENV = {
+  SUPABASE_URL: "https://kpnzyogxglkacepmoqed.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwbnp5b2d4Z2xrYWNlcG1vcWVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTU4MzgsImV4cCI6MjA5Nzc5MTgzOH0.4bOsvSZjqXm-XRz6B7kY_zlDBZi5hLHsBf2Ycu-1ch8"
+};
